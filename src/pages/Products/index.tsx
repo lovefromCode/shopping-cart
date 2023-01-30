83d828a224267ed16ad352dfec8f6757997e77c2
Prashant Maitra <prashantmaitra@smileadmins-MacBook-Pro.local>
2023-01-30 11:55:50 +0530

    remove usestate, usecontext useEffect

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React from 'react';
 import Navbar from '../../components/Navbar';
 import ProductFeed from './ProductFeed';
 
